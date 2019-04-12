e4de0231ee1a8026211b47d1059a36c56783eee5
Martin Adams <heymartinadams@users.noreply.github.com>
2019-04-12 11:57:06 -0700

    Swiftype code

--- src/search.js
+++ src/search.js
@@ -1,11 +1,12 @@
 // Add custom CSS to format search autocomplete
+/*
 setTimeout(() => {
   const autocompleteContainer = document.getElementsByClassName('st-default-autocomplete')[0]
   autocompleteContainer.className = 'st-autocomplete-container'
   const autocompleteResult = document.getElementsByClassName('st-autocomplete-results st-ui-autocomplete')[0]
   autocompleteResult.className = 'st-autocomplete-result'
 }, 2000)
-/*
+
 // Swiftype official code
 (function(w,d,t,u,n,s,e){w['SwiftypeObject']=n;w[n]=w[n]||function(){
 (w[n].q=w[n].q||[]).push(arguments);};s=d.createElement(t);
