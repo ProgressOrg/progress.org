111d53b36b520e4e1e1bc82c8a09e52eeaf231cc
Martin Adams <heymartinadams@users.noreply.github.com>
2019-04-12 11:49:00 -0700

    Swiftype code

--- src/search.js
+++ src/search.js
@@ -5,3 +5,10 @@ setTimeout(() => {
   const autocompleteResult = document.getElementsByClassName('st-autocomplete-results st-ui-autocomplete')[0]
   autocompleteResult.className = 'st-autocomplete-result'
 }, 2000)
+
+// Swiftype official code
+(function(w,d,t,u,n,s,e){w['SwiftypeObject']=n;w[n]=w[n]||function(){
+(w[n].q=w[n].q||[]).push(arguments);};s=d.createElement(t);
+e=d.getElementsByTagName(t)[0];s.async=1;s.src=u;e.parentNode.insertBefore(s,e);
+})(window,document,'script','//s.swiftypecdn.com/install/v2/st.js','_st');
+_st('install','sxxMbsn6QobC5tVp9rWi','2.0.0');
